daf5579e6569b6a7d512d532cd1d1783bf779ff9
Munkhbat <munkhbatg43@gmail.com>
2025-06-11 20:19:45 +0800

    dotenv

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,8 @@
 import jwt, { SignOptions, JwtPayload } from "jsonwebtoken";
 import dotenv from "dotenv";
-const JWT_SECRET = process.env.JWT_SECRET;
 dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in environment variables");
 }
